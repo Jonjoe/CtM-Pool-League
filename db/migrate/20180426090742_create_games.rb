c907f1bad7038648232bd9bc0a9a5c7b655182ea
jonjoe <me@jonjoe.io>
2018-04-26 10:12:44 +0100

    project init

---
+++ db/migrate/20180426090742_create_games.rb
@@ -0,0 +1,10 @@
+class CreateGames < ActiveRecord::Migration[5.2]
+  def change
+    create_table :games do |t|
+      t.references :winner, foreign_key: { to_table: :users }, optional: true
+      t.references :player1, foreign_key: { to_table: :users }
+      t.references :player2, foreign_key: { to_table: :users }
+      t.timestamps
+    end
+  end
+end
