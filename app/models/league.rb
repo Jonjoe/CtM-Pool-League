194b20218b9047b98c2f5ae83f69c9d1eb04d7db
jonjoe <me@jonjoe.io>
2018-04-26 13:20:22 +0100

    handling stats

--- app/models/league.rb
+++ app/models/league.rb
@@ -20,20 +20,18 @@ class League < ApplicationRecord
     end 
 
     def scores
-        if self.every_game_complete then
-            scores_hash = self.games.all.group(:winner_id).count
-            sorted_scores_hash = Hash[scores_hash.sort_by{|k, v| v}.reverse].to_a
+        scores_hash = self.games.all.group(:winner_id).count
+        sorted_scores_hash = Hash[scores_hash.except(nil).sort_by{|k, v| v}.reverse].to_a
 
-            scores_array = []
+        scores_array = []
 
-            sorted_scores_hash.each do |score|
-                scores_array.push({
-                    user: User.find(score[0]), 
-                    total: score[1]
-                })
-            end
-
-            return scores_array
+        sorted_scores_hash.each do |score|
+            scores_array.push({
+                user: User.find(score[0]), 
+                total: score[1]
+            })
         end
+
+        return scores_array
     end 
 end
