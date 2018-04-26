ee46ca46a4c9bef5ccab8c37c70b74ba9e1ef414
jonjoe <me@jonjoe.io>
2018-04-26 13:35:46 +0100

    create leagues

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,5 +1,33 @@
 class LeaguesController < ApplicationController
+    def index
+        @ongoing_leagues = League.where(winner_id: nil)
+        @completed_leagues = League.where.not(winner_id: nil)
+    end 
+
     def show
         @league = League.find(params[:id])
+    end 
+
+    def new
+        @league = League.new
+    end 
+
+
+    # ------------ CRUD Actions
+    def create
+        league = League.new(league_params)
+        league.owner_id = current_user.id
+        league.save
+
+        redirect_to leagues_path
+    end 
+
+    private 
+
+    def league_params
+        params.require(:league).permit(
+            :name,
+            :owner_id
+        )
     end 
 end
