7036c28746526e5ef2f31662715399433da0235b
jonjoe <me@jonjoe.io>
2018-04-26 14:31:23 +0100

    creating games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,45 @@
-class GamesController < ApplicationController 
+class GamesController < ApplicationController
+    # ------------ Template Methods    
+    def index
+        @games = Game.all
+    end 
+
+    def show
+        @game = Game.find(params[:id])
+    end 
+    
+    def new
+        @league = League.find(params[:league_id])
+        @users = User.where.not(role: "admin")
+        @game = Game.new
+    end 
+
+    # ------------ CRUD Actions
+    def create
+        game = Game.new(game_params)
+        game.league_id = params[:league_id]
+        game.save
+
+        redirect_to league_path(params[:league_id])
+    end
+
+    def update
+    end
+    
+    def destroy
+    end
+
+    # ------------ Bespoke Actions
+
+    private 
+
+    def game_params
+        params.require(:game).permit(
+            :name,
+            :winner_id,
+            :player1_id,
+            :player2_id,
+            :league_id,
+        )
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,22 +3,23 @@ Rails.application.routes.draw do
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
-
+  
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
-
+  
   resources :users, controller: "clearance/users", only: [:create] do
     resource :password,
-      controller: "clearance/passwords",
-      only: [:create, :edit, :update]
+    controller: "clearance/passwords",
+    only: [:create, :edit, :update]
   end
   
-  resources :games
   resources :users
+
   resources :leagues do
     get "/archive" => "leagues#archive", as: "archive"    
     get "/close" => "leagues#close", as: "close"    
-  end  
+    resources :games
+  end
 
   root 'home#index'
 end
